b04df5a5e56a1093cba0a7305dc48ad217e9ff77
stijn versluis <stijn.versluis@gmail.com>
2019-02-12 15:34:49 +0100

    all

---
+++ locations.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stijn versluis
+ * Date: 2/12/2019
+ * Time: 1:10 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Het Kroket Loket</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+<body>
+<div class="header">
+    <div class="container">
+        <div class="images">
+            <div class="image">
+
+            </div>
+        </div>
+        <!-- <div class="contacts">
+          <div class="contact">
+            <img class="num" src="img/phone_icon.png">
+            <p>+31 76 573 34 44</p>
+          </div>
+          <div class="contact">
+            <img class="mail" src="img/mail_icon.png">
+            <p>radiuscollege@rocwb.nl</p>
+          </div>
+        </div> -->
+    </div>
+</div>
+
+<!-- nav bar -->
+<div>
+    <div class="container">
+        <div class="navigation">
+
+            <div class="nav">
+                <h1>Het Kroket Loket</h1>
+                <a href="index.php">Home</a>
+                <a href="Products.php">Producten</a>
+                <a href="locations.php">Locaties</a>
+                <a href="order.php">Bestellen</a>
+                <a href="history.php">Onze Geschiedenis</a>
+                <div class="image">
+                    <a href="https://www.facebook.com/hetkroketloket/" target="blank">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="https://www.instagram.com/explore/locations/168811490255752/het-kroket-loket/" target="blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <input class="sign_in" type="button" value="Sign In">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="contactinfo">
+    <a href="https://www.google.com/maps/place/Het+Kroketloket/@51.9204093,4.4812633,15.77z/data=!4m5!3m4!1s0x47c4335c81d766b7:0xb7d83eeaa0c0ec59!8m2!3d51.9202674!4d4.4875253" target="blank">Rotterdam</a>
+    <a href="https://www.google.com/maps/place/Het+Kroketloket/@52.080026,4.315547,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5b72562f80a7d:0x4b74eb6887da9b13!8m2!3d52.0800227!4d4.3177357" target="blank">Den Haag</a>
+</div>
+
+
+</body></html>
---
+++ order.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stijn versluis
+ * Date: 2/12/2019
+ * Time: 1:11 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Het Kroket Loket</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+<body>
+<div class="header">
+    <div class="container">
+        <div class="images">
+            <div class="image">
+
+            </div>
+        </div>
+        <!-- <div class="contacts">
+          <div class="contact">
+            <img class="num" src="img/phone_icon.png">
+            <p>+31 76 573 34 44</p>
+          </div>
+          <div class="contact">
+            <img class="mail" src="img/mail_icon.png">
+            <p>radiuscollege@rocwb.nl</p>
+          </div>
+        </div> -->
+    </div>
+</div>
+
+<!-- nav bar -->
+<div>
+    <div class="container">
+        <div class="navigation">
+
+            <div class="nav">
+                <h1>Het Kroket Loket</h1>
+                <a href="index.php">Home</a>
+                <a href="Products.php">Producten</a>
+                <a href="locations.php">Locaties</a>
+                <a href="order.php">Bestellen</a>
+                <a href="history.php">Onze Geschiedenis</a>
+                <div class="image">
+                    <a href="https://www.facebook.com/hetkroketloket/" target="blank">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="https://www.instagram.com/explore/locations/168811490255752/het-kroket-loket/" target="blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <input class="sign_in" type="button" value="Sign In">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body></html>
