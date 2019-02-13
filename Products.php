d711fe5cad118404426e4fb81ba3c2e7f6c675c5
stijn versluis <stijn.versluis@gmail.com>
2019-02-13 13:24:17 +0100

    php products

--- Products.php
+++ Products.php
@@ -30,7 +30,7 @@ $kroketten = [
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
-<body>
+<body class="products">
 <div class="header">
     <div class="container">
         <div class="images">
