bd10a16ec96217cd49a9c4644cd210d69e001d39
stijn versluis <stijn.versluis@gmail.com>
2019-02-13 13:11:11 +0100

    aanpassing PHP producten

--- Products.php
+++ Products.php
@@ -6,19 +6,20 @@
  * Time: 1:05 PM
  */
 $kroketten = [
-        'Rotterdamse kaas kroket' . '<img class="image_Rotterdamsekaaskroket" src="https://duyt4h9nfnj50.cloudfront.net/sku/9ca4af7788db5a73a493b83aa8e7550a" alt="Rotterdamse kaas kroket">' => 3.00,
-    'Tomaat mozzerella kroket' . '<img class="image_Tomaatmozzerellakroket" src="https://duyt4h9nfnj50.cloudfront.net/sku/9ca4af7788db5a73a493b83aa8e7550a" alt="Tomaat mozzerella kroket">' => 3.00,
-    'Truffel paddenstoel kroket' . '<img class="image_Truffelpaddenstoelkroket" src="https://duyt4h9nfnj50.cloudfront.net/sku/5addc294cb5470b2d5bc0655c99d3e17" alt="Truffel paddenstoel kroket">' => 3.25,
-    'Garnalen kroket' . '<img class="image_Garnalenkroket" src="" alt="Garnalen kroket">' => 3.75,
-    'Tonijn kroket' . '<img class="image_Tonijnkroket" src="" alt="Tonijn kroket">' => 3.25,
-    'Rendang kroket' . '<img class="image_Rendangkroket" src="https://duyt4h9nfnj50.cloudfront.net/sku/dbdc0cc6b25d540a47b594d8ef38d83d" alt="Rendang kroket">' => 3.25,
-    'Smoor kroket' . '<img class="image_Smoorkroket" src="https://duyt4h9nfnj50.cloudfront.net/sku/594810df532e2bdfeda83735295678d1" alt="Smoor kroket">' => 3.25,
-    'Peking eend kroket' . '<img class="image_Pekingeendkroket" src="https://duyt4h9nfnj50.cloudfront.net/sku/15b05b5c60c24d2ef3d3d8881b8cb154" alt="Peking eend kroket">' => 3.25,
-    'Pittige kip kroket' . '<img class="image_Pittigekipkroket" src="https://duyt4h9nfnj50.cloudfront.net/sku/93d166adfe27f2a83655337f1c3e3079" alt="Pittige kip kroke">' => 3.25,
-    'Rundvlees kroket' . '<img class="image_Rundvleeskroket" src="https://duyt4h9nfnj50.cloudfront.net/sku/5482e88b8b9198cb9b578ae048431a28" alt="Rundvlees kroket">' => 2.50,
-    'Kroketten proeverij (6 kroketjes)' . '<img class="image_Krokkettenproeverij" src="https://duyt4h9nfnj50.cloudfront.net/sku/23cdfae05e40275059b7d23439fdf1e2" alt="Kroketten proeverij (6 kroketjes)">' => 6.50,
-    'Kroket van de maand' . '<img class="image_Kroketvandemaand" src="https://duyt4h9nfnj50.cloudfront.net/sku/d0e402f67e39b0e950291ef5ba33726e" alt="Kroket van de maand">' => 3.25,
+        "<a class='name' href=''><h2>Rotterdamse kaas kroket</h2> <img class='image_Rotterdamsekaaskroket' src='https://duyt4h9nfnj50.cloudfront.net/sku/9ca4af7788db5a73a493b83aa8e7550a' alt='Rotterdamse kaas kroket'></a>" => 3.00,
+    "<a class='name' href=''><h2>Tomaat mozzerella kroket</h2>  <img class='image_Tomaatmozzerellakroket' src='https://duyt4h9nfnj50.cloudfront.net/sku/9ca4af7788db5a73a493b83aa8e7550a' alt='Tomaat mozzerella kroket'></a>" => 3.00,
+    "<a class='name' href=''><h2>Truffel paddenstoel kroket</h2> <img class='image_Truffelpaddenstoelkroket' src='https://duyt4h9nfnj50.cloudfront.net/sku/5addc294cb5470b2d5bc0655c99d3e17' alt='Truffel paddenstoel kroket'></a>" => 3.25,
+    //'Garnalen kroket' . '<img class="image_Garnalenkroket" src="" alt="Garnalen kroket">' => 3.75,
+    //'Tonijn kroket' . '<img class="image_Tonijnkroket" src="" alt="Tonijn kroket">' => 3.25,
+    "<a class='name' href=''><h2>Rendang kroket</h2><img class='image_Rendangkroket' src='https://duyt4h9nfnj50.cloudfront.net/sku/dbdc0cc6b25d540a47b594d8ef38d83d' alt='Rendang kroket'></a>" => 3.25,
+    "<a class='name' href=''><h2>Smoor kroket</h2> <img class='image_Smoorkroket' src='https://duyt4h9nfnj50.cloudfront.net/sku/594810df532e2bdfeda83735295678d1' alt='Smoor kroket'></a>" => 3.25,
+    "<a class='name' href=''><h2>Peking eend kroket</h2> <img class='image_Pekingeendkroket' src='https://duyt4h9nfnj50.cloudfront.net/sku/15b05b5c60c24d2ef3d3d8881b8cb154' alt='Peking eend kroket'></a>" => 3.25,
+    "<a class='name' href=''><h2>Pittige kip kroket</h2> <img class='image_Pittigekipkroket' src='https://duyt4h9nfnj50.cloudfront.net/sku/93d166adfe27f2a83655337f1c3e3079' alt='Pittige kip kroke'></a>" => 3.25,
+    "<a class='name' href=''><h2>Rundvlees kroket</h2> <img class='image_Rundvleeskroket' src='https://duyt4h9nfnj50.cloudfront.net/sku/5482e88b8b9198cb9b578ae048431a28' alt='Rundvlees kroket'></a>" => 2.50,
+    "<a class='name' href=''><h2>Kroketten proeverij (6 kroketjes)</h2> <img class='image_Krokkettenproeverij' src='https://duyt4h9nfnj50.cloudfront.net/sku/23cdfae05e40275059b7d23439fdf1e2' alt='Kroketten proeverij (6 kroketjes)'></a>" => 6.50,
+    "<a class='name' href=''><h2>Kroket van de maand</h2> <img class='image_Kroketvandemaand' src='https://duyt4h9nfnj50.cloudfront.net/sku/d0e402f67e39b0e950291ef5ba33726e' alt='Kroket van de maand'></a>" => 3.25
 ]
+
 
 ?>
 <!DOCTYPE html>
